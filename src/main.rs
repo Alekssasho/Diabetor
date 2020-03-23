141e4ce82619ceac562c0a6d8758fefa34bde577
Alekssasho <aleksandar.angelovv@gmail.com>
2020-03-23 12:01:03 +0200

    Added bot example for High-level framework rlbot-gui

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,46 @@
-//! ATBA, short for Always Towards Ball Agent, is a bot that always drives
-//! blindly towards the ball no matter what is happening on the field (just
-//! like Dory from Finding Nemo).
-
 #![warn(future_incompatible, rust_2018_compatibility, rust_2018_idioms, unused)]
 #![cfg_attr(feature = "strict", deny(warnings))]
 #![warn(clippy::all)]
 
-use nalgebra::Vector2;
+use na::Vector2;
 use std::{error::Error, f32::consts::PI};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let rlbot = rlbot::init()?;
+    rlbot::run_bot(MyBot { player_index: 0 })
+}
 
-    let player_index = 0;
-    rlbot.start_match(&rlbot::MatchSettings::rlbot_vs_allstar("ATBA", "All-Star"))?;
-    rlbot.wait_for_match_start()?;
+struct MyBot {
+    player_index: usize,
+}
 
-    let mut packets = rlbot.packeteer();
-    loop {
-        let packet = packets.next()?;
+impl rlbot::Bot for MyBot {
+    fn set_player_index(&mut self, index: usize) {
+        self.player_index = index;
+    }
 
-        // check that match is started and not showing a replay.
-        // `packets.next_flatbuffer()` sleeps until the next packet is
-        // available, so this loop will not roast your CPU :)
-        if packet.ball.is_some() {
-            let input = get_input(&packet);
-            rlbot.update_player_input(player_index, &input)?;
-        }
+    fn tick(&mut self, packet: &rlbot::GameTickPacket) -> rlbot::ControllerState {
+        get_input(self.player_index, packet).unwrap_or_default()
     }
 }
 
-fn get_input(packet: &rlbot::GameTickPacket) -> rlbot::ControllerState {
-    let ball_loc = &packet.ball.as_ref().unwrap().physics.location;
-    let ball_loc = Vector2::new(ball_loc.x, ball_loc.y);
-
-    let car = &packet.players[0];
-    let car_loc = &car.physics.location;
-    let car_loc = Vector2::new(car_loc.x, car_loc.y);
+fn get_input(
+    player_index: usize,
+    packet: &rlbot::GameTickPacket,
+) -> Option<rlbot::ControllerState> {
+    let ball = packet.ball.as_ref()?;
+    let ball_loc = Vector2::new(ball.physics.location.x, ball.physics.location.y);
+    let car = &packet.players[player_index];
+    let car_loc = Vector2::new(car.physics.location.x, car.physics.location.y);
 
     let offset = ball_loc - car_loc;
     let desired_yaw = f32::atan2(offset.y, offset.x);
     let steer = desired_yaw - car.physics.rotation.yaw;
 
-    rlbot::ControllerState {
+    Some(rlbot::ControllerState {
         throttle: 1.0,
         steer: normalize_angle(steer).max(-1.0).min(1.0),
         ..Default::default()
-    }
+    })
 }
 
 /// Normalize an angle to between -PI and PI.
